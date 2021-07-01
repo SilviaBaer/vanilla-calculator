7fab9b669df5a48f1bc7d77fb651b0538a7dc284
SilviaBarcellini <77615477+SilviaBarcellini@users.noreply.github.com>
2021-07-01 18:20:52 +0200

    ✨ call the function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,8 @@
-alert("This works");
+let currentResult = 0;
+
+currentResult = currentResult + 10; 
+
+outputResult(currentResult, "");
 
 //var= data container where the value can change
 //const = data container where the value must not change
