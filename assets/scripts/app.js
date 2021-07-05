05f74b02b6d3cc531a0f3013473d1a35d87d8711
SilviaBarcellini <77615477+SilviaBarcellini@users.noreply.github.com>
2021-07-05 17:24:48 +0200

    ✨ add Custom Function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,6 +4,13 @@ currentResult = currentResult + 10;
 
 outputResult(currentResult, "");
 
+function add(num1, num2) {
+    const result = num1 + num2; 
+    alert("The result is " + result);
+} 
+
+add(1, 2); 
+
 //var= data container where the value can change
 //const = data container where the value must not change
 
