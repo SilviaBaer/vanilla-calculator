c3680335491c9963c3ce455eed32b5c1d4ea1ce3
SilviaBarcellini <77615477+SilviaBarcellini@users.noreply.github.com>
2021-07-05 17:29:34 +0200

    ✨ returning values instead of alerts

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,15 +1,15 @@
 let currentResult = 0;
 
-currentResult = currentResult + 10; 
+function add(num1, num2) {
+    const result = num1 + num2; 
+    return result
+} 
+
+currentResult = add(1, 2); 
 
 outputResult(currentResult, "");
 
-function add(num1, num2) {
-    const result = num1 + num2; 
-    alert("The result is " + result);
-} 
 
-add(1, 2); 
 
 //var= data container where the value can change
 //const = data container where the value must not change
