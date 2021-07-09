1669c0e4d92774e0fd34822347a3120ecf51c318
SilviaBarcellini <77615477+SilviaBarcellini@users.noreply.github.com>
2021-07-09 15:36:55 +0200

    ✨ apply logic to all operators

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -26,23 +26,53 @@ function add() {
 
 function subtract() {
     const enteredNumber = getUserInput();
+    const initialResult = currentResult;
     const calcDescription = `${currentResult} - ${enteredNumber}`//using ${} .toString is implicit
     currentResult -= enteredNumber;//or parseInt(userInput.value) for interger or parseFloat(userInput.value) for decimals too
     outputResult(currentResult, calcDescription);
+    const logEntry = {
+        operation: "SUBTRACT",
+        prevResult: initialResult,
+        number: enteredNumber,
+        newResult: currentResult
+    }
+    //logEntries.push(enteredNumber);//store values in an array
+    logEntries.push(logEntry);//store object in an array
+    console.log(logEntries);
 }
 
 function multiply() {
     const enteredNumber = getUserInput();
+    const initialResult = currentResult;
     const calcDescription = `${currentResult} * ${enteredNumber}`//using ${} .toString is implicit
     currentResult *= enteredNumber;//or parseInt(userInput.value) for interger or parseFloat(userInput.value) for decimals too
     outputResult(currentResult, calcDescription);
+    const logEntry = {
+        operation: "MULTIPLY",
+        prevResult: initialResult,
+        number: enteredNumber,
+        newResult: currentResult
+    }
+    //logEntries.push(enteredNumber);//store values in an array
+    logEntries.push(logEntry);//store object in an array
+    console.log(logEntries);
 }
 
 function divide() {
     const enteredNumber = getUserInput();
+    const initialResult = currentResult;
     const calcDescription = `${currentResult} / ${enteredNumber}`//using ${} .toString is implicit
     currentResult /= enteredNumber;//or parseInt(userInput.value) for interger or parseFloat(userInput.value) for decimals too
     outputResult(currentResult, calcDescription);
+    const logEntry = {
+        operation: "DIVIDE",
+        prevResult: initialResult,
+        number: enteredNumber,
+        newResult: currentResult
+    }
+    //logEntries.push(enteredNumber);//store values in an array
+    logEntries.push(logEntry);//store object in an array
+    console.log(logEntries);
 }
 
 addBtn.addEventListener("click", add); //no(), just keep in mind there is this function for now!
